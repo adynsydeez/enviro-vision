7d2978ee20542c4a4ebc2546582191e7eed558db
Manny <iamgoice@gmail.com>
2026-04-11 11:44:23 +1000

    pulled up a map lmao

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+function App() {
+  return (
+    <div style={{ height: "100vh", width: "100%" }}>
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={false}
+        style={{ height: "100%", width: "100%" }}
+      >
+        <TileLayer
+          attribution="&copy; OpenStreetMap contributors"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+}
+
+export default App;
